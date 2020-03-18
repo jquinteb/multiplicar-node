51f407b7db1b3d5d408e4eee02e6ebec3a2b14f0
Juan <juandavid.quintero@gmail.com>
2020-03-17 23:15:08 -0500

    Commit inicial

---
+++ app.js
@@ -0,0 +1,19 @@
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar.js');
+const argv = require('./config/yargs.js').argv;
+const colors = require('colors');
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: ${ colors.green(archivo)}`))
+            .catch(err => console.log(err));
+        break;
+    default:
+        console.log('comando no reconocido');
+        break;
+}
